9b8e2e79d5c3bedb1b83f9e6c0ec11bb8cffc71a
Descifle <descifle_itb@yahoo.com>
2020-06-04 13:03:08 -0400

    converted data over

--- js/controls.js
+++ js/controls.js
@@ -1,36 +1,6 @@
 $(document).ready(() => {
     //
-    //	carouFredSel banner
-    $($('.carousel')).on(() => {
-        $('#banner .carousel.main ul').carouFredSel({
-            auto: {
-                timeoutDuration: 8000
-            },
-            responsive: true,
-            prev: '.banner_prev',
-            next: '.banner_next',
-            width: '100%',
-            scroll: {
-                items: 1,
-                duration: 1000,
-                easing: "easeOutExpo"
-            },
-            items: {
-                width: '350',
-                height: 'variable',	//	optionally resize item-height
-                visible: {
-                    min: 1,
-                    max: 4
-                }
-            },
-            mousewheel: false,
-            swipe: {
-                onMouse: true,
-                onTouch: true
-            }
-        });
-    })
-
+    
     $('.sub-menu').hover(() => {
         $('.dropdown-menu').show()
     }/*, () => $('.dropdown-menu').hide()*/)
