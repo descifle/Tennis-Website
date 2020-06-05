2b232aa3276fd8165fe3a37ba793be4c37a384ba
Descifle <descifle_itb@yahoo.com>
2020-06-05 15:09:27 -0400

    added form/ fixed some nav issues/ updated php files

--- js/simple.js
+++ js/simple.js
@@ -11,5 +11,13 @@ $(document).ready(() => {
         }
     }/*, () => $('.dropdown').hide()*/)
 
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+    
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+
 });
 // end document.ready
